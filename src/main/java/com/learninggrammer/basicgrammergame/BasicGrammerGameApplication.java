d117d61eab0223f0b118e027b3d22e8808dc0103
Nikhil Mohan <nikhil_mohan@infosys.com>
2020-04-24 16:13:37 +0530

    Initial commit

---
+++ src/main/java/com/learninggrammer/basicgrammergame/BasicGrammerGameApplication.java
@@ -0,0 +1,13 @@
+package com.learninggrammer.basicgrammergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicGrammerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicGrammerGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learninggrammer/basicgrammergame/BasicGrammerGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learninggrammer.basicgrammergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicGrammerGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
